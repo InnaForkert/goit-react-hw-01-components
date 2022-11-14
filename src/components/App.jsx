2d0854f5c53a060de25923c6b6b009c8b26007aa
Inna Forkert <i.forkert@med.sumdu.edu.ua>
2022-11-14 16:59:18 +0200

    task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import user from './../user.json';
+import { Profile } from 'components/profile/Profile';
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   );
 };
---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,30 @@
+import css from "./Profile.module.css";
+
+export const Profile = ({ username, tag, location, avatar, stats }) => (
+    <div className={css.profile}>
+        <div className={css.description}>
+            <img
+                src={avatar}
+                alt="User avatar"
+                className={css.avatar}
+            />
+            <p className={css.name}>{username}</p>
+            <p className={css.tag}>@{tag}</p>
+            <p className={css.location}>{location}</p>
+        </div>
+        <ul className={css.stats}>
+            <li className={css.stat}>
+                <span className={css.label}>Followers</span>
+                <span className={css.num}>{stats.followers}</span>
+            </li>
+            <li className={css.stat}>
+                <span className={css.label}>Views</span>
+                <span className={css.num}>{stats.views}</span>
+            </li>
+            <li className={css.stat}>
+                <span className={css.label}>Likes</span>
+                <span className={css.num}>{stats.likes}</span>
+            </li>
+        </ul>
+    </div>
+)
