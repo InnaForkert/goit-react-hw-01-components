d7287a74a84add01e1f1a6deb9f66e3189e0fd29
Inna Forkert <i.forkert@med.sumdu.edu.ua>
2022-11-14 19:05:32 +0200

    task 2 style

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -5,15 +5,19 @@ export const Statistics = ({ title, stats }) => (
     <section className={css.statistics}>
         <h2 className={css.title}>{title}</h2>
         <ul className={css.statList}>
-            {stats.map(stat => <li key={stat.id} className={css.item}>
-                <span className={css.label}>{stat.label}</span>
-                <span className={css.percentage}>{stat.percentage}</span>
+            {stats.map(stat => <li key={stat.id} className={css.item} style={{ backgroundColor: getRandomHexColor() }}>
+                < span className={css.label} > {stat.label}</span>
+                <span className={css.percentage}>{stat.percentage}%</span>
             </li>)}
-        </ul>
-    </section>
+        </ul >
+    </section >
 )
 
 Statistics.propTypes = {
     title: PropTypes.string,
     stats: PropTypes.arrayOf(PropTypes.object)
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
