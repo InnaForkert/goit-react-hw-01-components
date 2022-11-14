cfca151c7f8137db537d8830adba071a4b104bac
Inna Forkert <i.forkert@med.sumdu.edu.ua>
2022-11-14 17:41:25 +0200

    bug: no statistics component visible

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
 import user from './../user.json';
+import data from './../data.json';
 import { Profile } from 'components/profile/Profile';
+import { Statistics } from 'components/statistics/Statistics';
 
 
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </>
   );
 };
---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,19 @@
+import css from "./Statistics.module.css";
+import PropTypes from "prop-types";
+
+export const Statistics = ({ title, stats }) => {
+    <section className={css.statistics}>
+        <h2 className={css.title}>{title}</h2>
+        <ul className={css.statList}>
+            {stats.map(stat => <li key={stat.id} className={css.item}>
+                <span className={css.label}>{stat.label}</span>
+                <span className={css.percentage}>{stat.percentage}</span>
+            </li>)}
+        </ul>
+    </section>
+}
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(PropTypes.object)
+}
