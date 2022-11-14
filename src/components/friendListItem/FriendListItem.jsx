cfce25a1e8d2ee4f2ae45e87601b84148c9bcaf9
Inna Forkert <i.forkert@med.sumdu.edu.ua>
2022-11-14 19:53:43 +0200

    task3 style

--- src/components/friendListItem/FriendListItem.jsx
+++ src/components/friendListItem/FriendListItem.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 export const FriendListItem = ({ avatar, name, isOnline }) => (
     <li className={css.item} >
-        <span className={css.status}>{isOnline ? 'online' : 'offline'}</span>
+        <span className={isOnline ? css.online : css.offline}></span>
         <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
         <p className={css.name}>{name}</p>
     </li>
